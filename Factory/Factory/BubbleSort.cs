a38a343ab29ec7c635966e8e2ffe3c35e43836d7
kadiralan <kadiralan021@gmail.com>
2017-05-28 15:15:33 +0300

    Added BubbleSort Class

---
+++ Factory/Factory/BubbleSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class BubbleSort : Algorithm
+    {
+        public override string Run()
+        {
+            return "Bubble Sort Çalışıyor";
+        }
+    }
+}
