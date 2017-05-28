7753113b606104940a1bb3e5caddee04f796a04f
kadiralan <kadiralan021@gmail.com>
2017-05-28 15:13:47 +0300

    Added SelectionSort Class

---
+++ Factory/Factory/SelectionSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class SelectionSort : Algorithm
+    {
+        public override string Run()
+        {
+            return "Selection Sort Çalışıyor";
+        }
+    }
+}
